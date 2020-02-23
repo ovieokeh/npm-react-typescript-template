554d73210373594aca817e0c8cc33d10d1d49df9
Ovie Okeh <kevinokeh@gmail.com>
2020-02-23 08:58:16 +0100

    switch to sass

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import url from '@rollup/plugin-url'
-import css from 'rollup-plugin-css-only'
-// import postcss from 'rollup-plugin-postcss'
+import sass from 'rollup-plugin-sass'
 import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
@@ -14,34 +12,14 @@ export default {
       file: pkg.main,
       format: 'cjs',
       exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
+      sourcemap: true,
+      strict: false
     }
   ],
   plugins: [
-    css({
-      // Filename to write all styles to
-      output: 'bundle.css',
-
-      // Callback that will be called ongenerate with two arguments:
-      // - styles: the contents of all style tags combined: 'body { color: green }'
-      // - styleNodes: an array of style objects: [{lang: 'css', content: 'body { color: green }'}]
-      output: function(styles, styleNodes) {
-        writeFileSync('bundle.css', styles)
-      },
-
-      // Disable any style output or callbacks
-      output: false,
-
-      // Default behaviour is to write all styles to the bundle destination where .js is replaced by .css
-      output: null
+    sass({
+      insert: true
     }),
-    url(),
     resolve(),
     typescript({
       objectHashIgnoreUnknownHack: true
